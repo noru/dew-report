52ff6d49153c2aaee1316667ae8e6f637cfb1245
xiuxiuxiu <nooooru@gmail.com>
2019-06-18 19:07:53 +0800

    dist

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  distDir: path.resolve(__dirname, './build'),
+  distDir: path.resolve(__dirname, './docs'),
   targetDir: path.resolve(__dirname, '../../src/main/webapp/react'),
 }
